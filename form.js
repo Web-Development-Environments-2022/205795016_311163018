c55fc1f5ff8e02b2b579df0bae34b054c1316f73
hodisan94 <dhodisan@gmail.com>
2022-05-11 18:04:35 +0300

    fixed the id for some element

--- form.js
+++ form.js
@@ -1,3 +1,15 @@
-function register_checking(){
-    
+
+var users = {}
+
+function register(){
+    let user_name = $('#User_Name').val()
+    let password = $('#pwd').val()
+    let pwd_confirm = $('#confirm').val()
+    let full_name = $('#full_name').val()
+    let email = $('#email').val()
+    let birthday = $('#birthday').val()
+
+    if (email.length < 6){
+        window.alert("password is too short")
+    }
 }
