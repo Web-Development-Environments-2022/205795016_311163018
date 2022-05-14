f5ee1bc7da19459d068eae32027b5d1da725b0a9
hodisan94 <dhodisan@gmail.com>
2022-05-14 21:02:45 +0300

    about dialog

--- form.js
+++ form.js
@@ -224,6 +224,55 @@ function setControls(u , d ,r ,l){
     return
 }
 
+
+// window.addEventListener('load', setup);
+
+// const get = document.getElementById.bind(document);
+// const query = document.querySelector.bind(document);
+// let modalRoot = get('modal-root');
+// let button = get('modal-opener');
+// let modal = query('.modal');
+
+
+// function setup() {
+  
+//   let modalRoot = get('modal-root');
+//   let button = get('modal-opener');
+//   let modal = query('.modal');
+  
+//   modalRoot.addEventListener('click', rootClick);
+//   button.addEventListener('click', openModal);
+//   modal.addEventListener('click', modalClick);
+  
+//   function rootClick() {
+//     modalRoot.classList.remove('visible');
+//   }
+  
+//   function openModal() {
+//     modalRoot.classList.add('visible');
+//   }
+  
+//   function modalClick(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     e.stopImmediatePropagation();
+//     return false;
+//   }
+  
+// }
+
+
+function openAbout() {
+    $('#About').show();
+
+
+	document.getElementById("dialog").showModal();
+}
+
+function closeAbout() {
+	document.getElementById("dialog").close();
+}
+
 function showsettings(){
 
     $('#Welcome').hide();
